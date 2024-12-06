0d0240fbbb521ff1fef50b24c977d2b2ad2e105e
misrasanat <misrasanat123@gmail.com>
2024-12-05 18:58:56 -0800

    update count in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(12340000)
+  const [count, setCount] = useState(123)
 
   return (
     <>
